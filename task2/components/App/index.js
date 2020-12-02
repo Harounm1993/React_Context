1586789e2c9511c993fdf24345b0f69d65c23ce8
P2586846 <mahdiharoun1993@gmail.com>
2020-12-02 16:01:21 +0000

    task2

--- task2/components/App/index.js
+++ task2/components/App/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import Header from "../Header";
 import Footer from "../Footer";
 import Article from "../Article";
+import {ThemeProvider, useThemeContext} from "../../themeContext"
 
 import { siteTitle, conTitle, conText } from "../../constants";
 
 import { theme } from "../../theme";
 
 function App() {
+
+ 
+const theme = useThemeContext
   return (
     <div className="main-container" style={theme.light}>
       <Header title={siteTitle}></Header>
